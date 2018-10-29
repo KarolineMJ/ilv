1fb490143b9576ea66f40ccbbac2d24911a593bd
KarolineMJ <karoline_mj@hotmail.com>
2018-10-29 14:41:12 +0100

    styled and tested

--- js/script.js
+++ js/script.js
@@ -1,5 +1,50 @@
 "use strict";
+let burgermenu = document.querySelector(".container");
+let navbar = document.querySelector(".navigation");
 
-function myFunction(x) {
-    x.classList.toggle("change");
+burgermenu.addEventListener("click", openNav)
+
+function openNav() {
+    burgermenu.classList.toggle("change");
+    navbar.classList.toggle("show");
 }
+
+// read more button
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.querySelector(".myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 3 seconds
+}
+
